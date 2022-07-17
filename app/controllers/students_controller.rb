3fd1fd87168a9143788a59a3ac3ef313a270b64a
Devin Garcia <devingarcia5423@gmail.com>
2022-07-17 19:30:18 -0400

    commit tree 5bfd96ed019c0d0aa8036068960e6f582d47e637

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params.has_key?(:name)
+      students = Student.where(first_name: params[:name].capitalize()).or(Student.where(last_name: params[:name].capitalize()))
+    else
+      students = Student.all
+    end
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
